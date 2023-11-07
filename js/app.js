28c9868b610cf7f315ee896c449a69a542863199
RerdPando <willyataidesnew88@gmail.com>
2023-11-07 12:52:30 -0300

    Atualização 1.2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
-let listaDeProdutos = []
+let valorDoCarrinho = 0;
 
-function adicionar(){
-    let produto = document.getElementById('produto').value;
-    let precoDoProduto = document.getElementById('produto').preco;
+//Adiciona o produto ao carrinho (estudar função).
+function adicionar() {
 
-    let classe = document.querySelector(".carrinho__produtos__produto");
-    let novoElemento = document.createElement(`<span class="texto-azul">1x</span> ${produto} <span class="texto-azul">R$${precoDoProduto}</span>`);
+    //Armazena o valor inserida no campo "quantidade"
+    let quantidadeDeProdutos = document.getElementById('quantidade').value;
 
-    classe.appendChild(novoElemento);
+    //Verificação para caso o cliente não adicione a quantidade.
+    if (quantidadeDeProdutos => 0) {
+        //Pegar o nome do produto para adicionar no novo elemento.
+        let produto = document.getElementById('produto').value;
+
+        //Pegar o preço do produto para adicionar no novo elemento.
+        let pegarPreco = document.getElementById('produto');
+        precoDoProduto = pegarPreco.options[pegarPreco.selectedIndex].getAttribute("preco");
+        precoDoProduto = parseInt(precoDoProduto);
+
+        //Adiciona o produto a lista do carrinho 
+        let AdicionarNovoPro = document.getElementById('lista-produtos');
+        novoProduto = document.createElement('span');
+        novoProduto.innerHTML = `<span class="texto-azul">${quantidadeDeProdutos}x</span> ${produto} <span class="texto-azul">R$${precoDoProduto}</span>`;
+
+        AdicionarNovoPro.appendChild(novoProduto);
+
+        //Adiciona o valor total do produtos do carrinho.
+        let novoValor = valorDoCarrinho + (precoDoProduto * quantidadeDeProdutos);
+        valorDoCarrinho = novoValor;
+        document.getElementById('valor-total').textContent = `R$${novoValor}`;
+    } else {
+        alert('Adicione uma quantidade para adicionar!');
+    }
 };
