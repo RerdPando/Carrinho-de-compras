8a2022bec22830f632d6abb82455291847d22f3e
RerdPando <willyataidesnew88@gmail.com>
2023-11-11 11:18:17 -0300

    atualização Final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let valorAtual = 0;
+
 //Adiciona o produto ao carrinho (estudar função).
 function adicionar() {
 
@@ -7,33 +9,27 @@ function adicionar() {
     let valorDoProduto = produto.split('R$')[1];
     let quantidadeDeProdutos = document.getElementById('quantidade').value;
 
+    //verifica se a quantidade foi adicionada
+    if(quantidadeDeProdutos <= 0){
+      alert('Coloque uma quantidade!');
+      return;
+    };
     //set subtotal 
     let subTotal = valorDoProduto * quantidadeDeProdutos;
 
     let produtosDoCarrinho = document.getElementById('lista-produtos');
     produtosDoCarrinho.innerHTML += `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidadeDeProdutos}x</span> ${nomeDoProduto} <span class="texto-azul">R$${valorDoProduto}</span>
+    <span class="texto-azul">${quantidadeDeProdutos}x</span> ${nomeDoProduto} <span class="texto-azul">R$${subTotal}</span>
   </section>`;
 
     let valorDoCarrinho = document.getElementById('valor-total');
-    let valorAtual = valorDoCarrinho.value.split('$')[1];
-    let valorFinal = valorAtual + subTotal;
-    valorDoCarrinho.textContent = `R$${valorFinal}`;
+    valorAtual = valorAtual + subTotal;
+    document.getElementById('quantidade').value = 0;
+    valorDoCarrinho.textContent = `R$${valorAtual}`;
 };
 
 function limpar() {
-
-    //limpa a lista de produtos no carrinho
-    let classPricipal = document.querySelector('.carrinho__produtos__produto');
-    let limparSpans = classPricipal.querySelectorAll('span');
-
-    //limpa o valor total do carrinho
-    document.getElementById('valor-total').textContent = 'R$0.00';
-
-    limparSpans.forEach(function (span) {
-        classPricipal.textContent = '';
-        classPricipal.remove(span);
-    });
-
-
-};
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('quantidade').value = 0;
+    document.getElementById('valor-total').textContent = `R$ 0`;
+}
